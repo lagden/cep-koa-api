3b94c2f18b2ab20165364ec57bf3fa72e4708aca
Thiago Lagden <lagden@gmail.com>
2015-05-12 06:25:53 -0300

    added redis cloud

--- index.js
+++ index.js
@@ -2,19 +2,36 @@
 
 'use strict';
 
+var config       = require('./.redis.json');
+var rHost        = process.env.HOST || config.host;
+var rPort        = process.env.PORT || config.port;
+var rPasswd      = process.env.PASSWD || config.passwd;
+var env          = process.env.NODE_ENV || 'development';
+
 var app          = require('koa')();
 var responseTime = require('koa-response-time');
 var compress     = require('koa-compress');
 var logger       = require('koa-logger');
 var Router       = require('koa-router');
 var router       = new Router(app);
-var redisClient  = require('redis').createClient();
+
+var redisClient;
+if ('test' !== env) {
+  redisClient  = require('redis').createClient(
+    rPort,
+    rHost, {
+      no_ready_check: true,
+      auth_pass: rPasswd
+    });
+} else {
+  redisClient  = require('redis').createClient();
+}
+
 var wrapper      = require('co-redis');
 var redisCo      = wrapper(redisClient);
 var genify       = require('thunkify-wrap').genify;
 var correio      = require('io-cep');
 var consulta     = genify(correio);
-var env          = process.env.NODE_ENV || 'development';
 
 module.exports = app;
 
